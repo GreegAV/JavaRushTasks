8671c538d3f0af77b0f60c006efdf06c993d5800
GreegAV <greegav@gmail.com>
2017-04-20 21:10:17 +0300

    Another training day

--- 1.JavaSyntax/src/com/javarush/task/task06/task0601/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0601/Cat.java
@@ -13,9 +13,12 @@ package com.javarush.task.task06.task0601;
 
 public class Cat {
 
+    protected void finalize() throws Throwable {
 
+    }
     //напишите тут ваш код
     public static void main(String[] args) {
+
 
     }
 }
