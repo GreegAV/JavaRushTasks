39328ebc7ccafacd0f5e551e0791ba590f294627
GreegAV <greegav@gmail.com>
2017-04-30 22:33:14 +0300

    task0707, task 0708

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0708/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task07.task0708;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+/* 
+Самая длинная строка
+1. Создай список строк.
+2. Считай с клавиатуры 5 строк и добавь в список.
+3. Используя цикл, найди самую длинную строку в списке.
+4. Выведи найденную строку на экран.
+5. Если таких строк несколько, выведи каждую с новой строки.
+
+
+Требования:
+1. Объяви переменную типа ArrayList (список строк) и сразу проинициализируй ee.
+2. Программа должна считывать 5 строк с клавиатуры и записывать их в список.
+3. Программа должна выводить самую длинную строку на экран.
+4. Если есть несколько строк с длиной равной максимальной, то нужно вывести каждую из них с новой строки.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        ArrayList<String> list = new ArrayList<>();
+        Scanner scn = new Scanner(System.in);
+        int strlenght=0, maxnumstr=0;
+        for (int i = 0; i <5 ; i++) {
+            list.add(i,scn.nextLine());
+            if (strlenght<list.get(i).length()) {
+                strlenght=list.get(i).length();
+                maxnumstr=i;
+            }
+        }
+        for (int i = 0; i < 5; i++) {
+            if (list.get(i).length()==list.get(maxnumstr).length()) System.out.println(list.get(i));
+        }
+
+        //напишите тут ваш код
+    }
+}
