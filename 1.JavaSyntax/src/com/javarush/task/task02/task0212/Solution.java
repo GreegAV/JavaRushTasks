c3ae07b7b1f5aa173274b4710278b2e8a2251d65
GreegAV <greegav@gmail.com>
2017-07-08 23:45:56 +0300

    Level 2 completed

---
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0212/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task02.task0212;
+
+/* 
+Великолепная восьмерка
+Создай 10 переменных типа Cat и 8 объектов типа Cat.
+
+
+Требования:
+1. Программа не должна выводить текст на экран.
+2. В методе main должно быть 10 переменных типа Cat.
+3. В методе main 8 переменных должны быть проинициализированы.
+4. В классе Cat не должно быть переменных.
+5. В классе Cat не должно быть методов.
+*/
+public class Solution {
+    public static void main(String[] args) {
+        Cat cat0;
+        Cat cat1;
+        Cat cat2;
+        Cat cat3;
+        Cat cat4;
+        Cat cat5;
+        Cat cat6;
+        Cat cat7;
+        Cat cat10;
+        //напишите тут ваш код
+        Cat cat8 = new Cat();
+        Cat cat81 = new Cat();
+        Cat cat82 = new Cat();
+        Cat cat83 = new Cat();
+        Cat cat84 = new Cat();
+        Cat cat85 = new Cat();
+        Cat cat86 = new Cat();
+        Cat cat87 = new Cat();
+        Cat cat9;
+        //напишите тут ваш код
+    }
+
+    public static class Cat {
+
+    }
+}
