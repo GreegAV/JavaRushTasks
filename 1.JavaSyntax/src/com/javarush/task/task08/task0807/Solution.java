a5b1825762568035552ab677db38f2fc4e98fb40
GreegAV <greegav@gmail.com>
2017-07-11 02:00:59 +0300

    Level 8, Task 0807,0808,0809,0810,0811,0812

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0807/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task08.task0807;
+
+import java.util.*;
+
+/* 
+LinkedList и ArrayList
+Нужно создать два списка – LinkedList и ArrayList.
+
+
+Требования:
+1. Программа не должна выводить текст на экран.
+2. Программа не должна считывать значения с клавиатуры.
+3. Программа должна содержать только три метода.
+4. Метод createArrayList() должен создавать и возвращать список ArrayList.
+5. Метод createLinkedList() должен создавать и возвращать список LinkedList.
+*/
+
+public class Solution {
+    public static Object createArrayList() {
+        ArrayList arrayList = new ArrayList();
+        return arrayList;
+        //напишите тут ваш код
+
+    }
+
+    public static Object createLinkedList() {
+        LinkedList linkedList = new LinkedList();
+        return linkedList;
+        //напишите тут ваш код
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0812/Solution.java
@@ -0,0 +1,47 @@
+package com.javarush.task.task08.task0812;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/* 
+Cамая длинная последовательность
+1. Создай список чисел.
+2. Добавь в список 10 чисел с клавиатуры.
+3. Вывести на экран длину самой длинной последовательности повторяющихся чисел в списке.
+
+Пример для списка 2, 4, 4, 4, 8, 8, 9, 12, 12, 14:
+3
+
+Искомое значение равно 3, т.к. самая длинная последовательность повторяющихся чисел состоит из трех четверок.
+
+
+Требования:
+1. Программа должна выводить число на экран.
+2. Программа должна считывать значения с клавиатуры.
+3. В методе main объяви переменную типа ArrayList с типом элементов Integer и сразу проинициализируй ee.
+4. Программа должна добавлять в коллекцию 10 чисел, согласно условию.
+5. Программа должна выводить на экран длину самой длинной последовательности повторяющихся чисел в списке.
+*/
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        //напишите тут ваш код
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i <10 ; i++) {
+            arrayList.add(scanner.nextInt());
+        }
+
+        int posl = 0;
+        int count = 1;
+        for (int i = 0; i < arrayList.size() - 1; i++) {
+            if (arrayList.get(i) == arrayList.get(i + 1)) count++;
+            else {
+                if (posl < count) posl = count;
+                count = 1;
+            }
+        }
+        if (posl < count) posl = count;
+        System.out.println(posl);
+    }
+}
